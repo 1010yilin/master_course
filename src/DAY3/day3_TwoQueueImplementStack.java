516eaf0c611291e0e7df117306b76cdbe76597ee
1010yilin <yi1010li@gmail.com>
2023-07-28 21:31:17 +0800

    test1

--- src/DAY3/day3_TwoQueueImplementStack.java
+++ src/DAY3/day3_TwoQueueImplementStack.java
@@ -19,7 +19,33 @@ public class day3_TwoQueueImplementStack {
 		stackQueue.offer(value);
 	}
 
+	public T poll(){
+		//留最后一个数在原来的队列里面弹出，其他挪到新队列中
+		while (stackQueue.size() > 1){
+			stackHelper.offer(stackQueue.poll());
+		}
+		T ans = stackQueue.poll();
+		Queue<T> tmp = stackQueue;
+		stackQueue = stackHelper;
+		stackHelper = tmp;
+		return ans;
+	}
 
+	public T peek(){
+		//将最后一个值取出再加入
+		while (stackQueue.size()>1){
+			stackHelper.offer(stackQueue.poll());
+		}
+		T ans = stackQueue.poll();
+		stackHelper.offer(ans);
+		Queue<T> tmp = stackQueue;
+		stackQueue = stackHelper;
+		stackHelper = tmp;
+		return ans;
+	}
+	  public boolean isEmpty() {
+		  return stackQueue.isEmpty();
+	  }
   }
 
 
